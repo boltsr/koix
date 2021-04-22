c67a5c34d2d8993937a57069785a64088194f5ad
Salman Arshad <k163898@nu.edu.pk>
2021-04-22 22:41:49 +0500

    comit

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ console.log("hello")
 const moment = require("moment");
 
 console.log(moment())
+const { exec } = require("child_process");
+
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
