f316bb90568011e767ff413b59be9edaa345b93e
itsimpledeveloper917 <itsimpledeveloper917@gmail.com>
2021-05-25 22:01:42 -0400

    get user input

--- index.js
+++ index.js
@@ -2,14 +2,31 @@
 var process = require('child_process');
 var colors = require('colors');
 var path = require('path');
-console.log("here-1")
-var templateDir = path.join(__dirname, 'cra-template-koix');
 
-// console.log(x);
-// templateDir  = __dirname+"\"
-console.log("here0")
+const COMPONENT_LIST = ['navbar', 'newsfeed']
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var user_select_comps = {}
+console.log("select components you want to add ".green)
+readline.question('navbar (yes)', name => {
+    console.log(`Hey there ${name}!`);
+    if(name) {
+        user_select_comps.navbar = true
+    }else{
+        user_select_comps.navbar = false
+    }
+    readline.close();
+});
+// readline.question('navbar (yes)', name => {
+//     console.log(`Hey there ${name}!`);
+//     readline.close();
+// });
+/*
+var templateDir = path.join(__dirname, 'cra-template-koix');
 var cmd = process.spawn("npx", ["create-react-app","myapp8","--template",`file:${templateDir}`]);
-console.log("here1")
+
 cmd.stdout.on('data', function(output){
     dd=output.toString()
     // const dataJson = eval(`(${dd})`);
@@ -18,6 +35,11 @@ cmd.stdout.on('data', function(output){
 
 cmd.on('close', function(){
     console.log('Finished'.green);
+    console.log("select components you want to add ".green)
+    readline.question('navbar (yes)', name => {
+        console.log(`Hey there ${name}!`);
+        readline.close();
+    });
 });
 
 //Error handling
@@ -26,3 +48,4 @@ cmd.stderr.on('data', function(err){
     console.log(err.toString().yellow)
     // console.log(err);
 });
+*/
