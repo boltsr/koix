c43f5ee7dbf90520b64314f01ccd56e6a95f28ef
itsimpledeveloper917 <itsimpledeveloper917@gmail.com>
2021-05-27 14:43:44 -0400

    update app.js

--- test/src/App.js
+++ test/src/App.js
@@ -8,37 +8,23 @@ import "react-circular-progressbar/dist/styles.css";
 // import "assets/css/fontawesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
-import BasicStyle from "theme/basicStyle";
-import GlobalStyle from "theme/globalStyle";
-import GlobalModalStyle from "theme/globalModalStyle";
 import MyRoute from "service/MyRoute";
 import Leaderboard from "containers/Leaderboard";
 import DataContextContainer from "contexts/DataContextContainer";
-import AnnounceContextContainer from "contexts/AnnounceContextContainer";
-import ScrollContextContainer from "contexts/ScrollContextContainer";
 
 function App() {
   return (
     <div className="App">
-      {/* <Scrollbars autoHide style={{ flex: 1 }}> */}
-        <BasicStyle />
-        <GlobalStyle />
-        <GlobalModalStyle />
-        <Router>
-          <Switch>
-            <DataContextContainer>
-              <ScrollContextContainer>
-                <AnnounceContextContainer>
-                  <MyRoute
-                    exact
-                    component={Leaderboard}
-                  />
-                </AnnounceContextContainer>
-              </ScrollContextContainer>
-            </DataContextContainer>
-          </Switch>
-        </Router>
-      {/* </Scrollbars> */}
+      <Router>
+        <Switch>
+          <DataContextContainer>
+            <MyRoute
+              exact
+              component={Leaderboard}
+            />
+          </DataContextContainer>
+        </Switch>
+      </Router>
     </div>
   );
 }
