245deeb4cbf568fcd2a6559f08f0d94098adfc22
itsimpledeveloper917 <itsimpledeveloper917@gmail.com>
2021-05-26 21:51:17 -0400

    test file copy function

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 var process = require('child_process');
 var colors = require('colors');
 var path = require('path');
+const { copyFile } = require('./utils');
 const prompt = require('prompt-sync')({sigint: true});
 
 const COMPONENT_LIST = ['navbar', 'newsfeed']
-
-const select_component = () => {
+const DEBUG_MODE = true
+const select_component = async () => {
     var user_select_comps = {}
     console.log("select components you want to add ".green)
     let str_navbar = prompt('navbar (yes) ');
@@ -17,26 +18,35 @@ const select_component = () => {
         user_select_comps.navbar = false
     }
     console.log({user_select_comps})
+    try{
+        await copyFile('file.json', './test/file.json')
+    }catch(err){
+        console.log("file copy error", err)
+    }
     console.log('Finished'.green);
 }
 
-var templateDir = path.join(__dirname, 'cra-template-koix');
-var cmd = process.spawn("npx", ["create-react-app","myapp8","--template",`file:${templateDir}`]);
-
-cmd.stdout.on('data', function(output){
-    dd=output.toString()
-    // const dataJson = eval(`(${dd})`);
-    console.log(dd)
-});
-
-cmd.on('close', function(){
-    // console.log('Finished'.green);
+if(DEBUG_MODE) {
     select_component()
-});
-
-//Error handling
-cmd.stderr.on('data', function(err){
-    // console.log("error".red)
-    console.log(err.toString().yellow)
-    // console.log(err);
-});
+}else{
+    var templateDir = path.join(__dirname, 'cra-template-koix');
+    var cmd = process.spawn("npx", ["create-react-app","myapp8","--template",`file:${templateDir}`]);
+    
+    cmd.stdout.on('data', function(output){
+        dd=output.toString()
+        // const dataJson = eval(`(${dd})`);
+        console.log(dd)
+    });
+    
+    cmd.on('close', function(){
+        // console.log('Finished'.green);
+        select_component()
+    });
+    
+    //Error handling
+    cmd.stderr.on('data', function(err){
+        // console.log("error".red)
+        console.log(err.toString().yellow)
+        // console.log(err);
+    });
+}
--- utils.js
+++ utils.js
@@ -1,13 +1,9 @@
 const fs = require('fs');
 
-// File destination.txt will be created or overwritten by default.
-fs.copyFile('source.txt', 'destination.txt', (err) => {
-  if (err) throw err;
-  console.log('source.txt was copied to destination.txt');
-});
-
 const copyFile = async (from_path, to_path) => {
   return new Promise((resolve, reject) => {
+    console.log({from_path})
+    console.log({to_path})
     fs.copyFile(from_path, to_path, (err) => {
       if (err) return reject(err);
       resolve(true)
